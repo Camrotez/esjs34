c7727635c95d9bb57c734d17712e89b6cfebd480
Luciano <lunick997@gmail.com>
2023-06-21 11:06:42 +0200

    fix

--- exercise.js
+++ exercise.js
@@ -1,7 +1,13 @@
 function printAsyncName(callback, name){
     let param = setInterval(callback ,1000);
-    setTimeout(() => clearInterval(param),2000)
+    setTimeout(() => console.log(name),2000);
+    setTimeout(() => clearInterval(param),2000);
+    
 }
 
-printAsyncName(() => console.log("Hello"));
+function call(){
+    console.log("hello")
+}
+
+printAsyncName(call,"luciano")
 
